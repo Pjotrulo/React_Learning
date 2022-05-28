0240b9b33e932d26a9cd2b973fd5369579680c00
Pjotrulo <piotrgdwn@gmail.com>
2022-05-28 15:43:57 +0200

    Learning React

--- reactapp/src/components/Todo.js
+++ reactapp/src/components/Todo.js
@@ -9,14 +9,18 @@ function Todo(props) {
         setModalIsOpen(true);
     }
 
+    function closeModalHandler() {
+        setModalIsOpen(false);
+    }
+
     return (
         <div className="card">
             <h2>{props.text}</h2> 
             <div className="actions">
                 <button className="btn" onClick={deleteHandler}>Delete</button>
             </div>
-            {modalIsOpen ? <Modal /> : null}
-            {modalIsOpen && <Backdrop />}
+            {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/> }
+            {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
         </div>
     );
 }
